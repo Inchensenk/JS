302ac641a3ff735054b519b1411d932c84b7fa06
sofoyan gevork <aab81@bk.ru>
2023-04-26 19:49:23 +0300

    ...

---
+++ YOUTUBE/Prototype inheritance/01/1.js
@@ -0,0 +1,25 @@
+function Auto(brand, price, gas){
+   this.brand=brand;
+   this.price=price;
+   this.gas=gas;
+   this.drive = function()
+   {
+        if(this.gas > 0)
+        {
+            this.gas = this.gas - 20;
+            return console.log(this.gas);
+        }else{
+            console.log('бензин закончился');
+        }
+   }
+}
+
+const bmw = new Auto('bmw', '100,000','100');
+const nissan = new Auto('nissan', '40,000','100');
+
+bmw.drive();//80
+bmw.drive();//60
+bmw.drive();//40
+bmw.drive();//20
+bmw.drive();//0
+bmw.drive();//бензин закончился
