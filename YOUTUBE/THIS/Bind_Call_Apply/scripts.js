2815b6788c02fe3c9a5efb495bfd9dc6ace15e14
sofoyan gevork <aab81@bk.ru>
2023-04-26 10:03:18 +0300

    ...

--- YOUTUBE/THIS/Bind_Call_Apply/scripts.js
+++ YOUTUBE/THIS/Bind_Call_Apply/scripts.js
@@ -12,11 +12,14 @@ const motorBike = {
 
 
 
-
+/*
 const autoDrive = auto.drive.bind(auto);
 autoDrive();//{ brand: 'BMW', drive: [Function: drive] }// в хроме 'Заведем наш BMW'
 
 const motorDrive = auto.drive.bind(motorBike);
 motorDrive();//{ brand: 'Suzuki' }// в хроме 'Заведем наш Suzuki'
 
-const motorDrive1 = auto.drive.bind({brand: 'Корабл'});// в хроме 'Заведем наш Корабл'
+const motorDrive1 = auto.drive.bind({brand: 'Корабл'});// в хроме 'Заведем наш Корабл'*/
+
+const header = document.querySelector('h2'); 
+console.log(header);
