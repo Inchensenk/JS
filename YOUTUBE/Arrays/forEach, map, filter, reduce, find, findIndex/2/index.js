865d55511c88d456ec242521b90d773bba346f56
sofoyan gevork <aab81@bk.ru>
2023-04-26 12:19:25 +0300

    ...

---
+++ YOUTUBE/Arrays/forEach, map, filter, reduce, find, findIndex/2/index.js
@@ -0,0 +1,10 @@
+const people = [
+    {name: 'Владимир', age: 25, budget: 40000},
+    {name: 'Елена', age: 17, budget: 3400},
+    {name: 'Игорь', age: 49, budget: 50000},
+    {name: 'Михаил', age: 15, budget: 1800},
+    {name: 'Василиса', age: 24, budget: 25000},
+    {name: 'Виктория', age: 38, budget: 23000},
+];
+
+
