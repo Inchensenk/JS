e046987035eead1346ed50ca0d939ae76d2dd023
sofoyan gevork <aab81@bk.ru>
2023-04-26 10:44:30 +0300

    ...

--- YOUTUBE/THIS/Bind_Call_Apply/scripts.js
+++ YOUTUBE/THIS/Bind_Call_Apply/scripts.js
@@ -22,7 +22,20 @@ console.log(payCount(1000));*/
 /*//меняем процент чаевых на 20%
 const payCount = bill.calculate.bind({tip: 0.2});
 console.log(payCount(1000));*/
-
+/*
 //тоже самое что и выше
 const payCount = bill.calculate.bind({tip: 0.2}, 1000);
-console.log(payCount());
+console.log(payCount());*/
+/*
+//Контекст привязывается 1 раз и мы не можем его повторно перепривязывать поэтому в выводе 1100
+const payCount = bill.calculate.bind(bill).bind({tip: 0.2}).bind({tip: 0.5});
+console.log(payCount(1000));
+*/
+
+const pay = bill.calculate.bind(bill);
+const payCount = bill.calculate.call(bill, 1000);
+const payCount2 = bill.calculate.apply(bill, [1000]);
+
+console.log(pay(1000));//1100
+console.log(payCount);//1100
+console.log(payCount2);//1100
