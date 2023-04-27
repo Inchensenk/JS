a8fc233877b8375adaf6dd5b175855f0d418c5a5
sofoyan gevork <aab81@bk.ru>
2023-04-27 16:47:59 +0300

    ....

--- YOUTUBE/DOM/01/DOM.js
+++ YOUTUBE/DOM/01/DOM.js
@@ -11,3 +11,26 @@ header.textContent = 'DOM (Doccument Object Model)';
 console.log(header);
 //Добавить H1 перед DIV с классом wrapper
 div.insertAdjacentElement('beforebegin', header);
+//Создать список ul и добавить в него три элемента с текстом 'один, два, три'
+const ul = `
+    <ul>
+        <li>один</li>
+        <li>два</li>
+        <li>три</li>
+    </ul>   
+`
+//Поместить список внутрь элемента wrapper
+div.innerHTML = ul;
+/****************************************************************************************
+ ***************************************************************************************
+ ****************************************************************************************/
+//Создать изобрапжение
+const img = document.createElement('img');
+//Добавить атрибут source
+img.src = 'https://picsum.photos/240';
+//Добавить атрибут width=240
+img.width = 240;
+//добавить класс super
+img.classList.add('super');
+
+img.alt = 'Super Man'
