0b4fc21d543309ce98557ec383f59aca9dd2ac53
sofoyan gevork <aab81@bk.ru>
2023-05-01 11:38:29 +0300

    DOM

--- YOUTUBE/DOM/01/DOM.js
+++ YOUTUBE/DOM/01/DOM.js
@@ -1,6 +1,6 @@
 //Создать div
 const div = document.createElement('div');
-//Добавить класс wrapper
+//Добавить к нему класс wrapper
 div.classList.add('wrapper');
 //Поместить его внутрь тега body
 const body = document.body;
@@ -24,13 +24,76 @@ div.innerHTML = ul;
 /****************************************************************************************
  ***************************************************************************************
  ****************************************************************************************/
-//Создать изобрапжение
+//I Создать изобрапжение
 const img = document.createElement('img');
-//Добавить атрибут source
+//I.1. Добавить атрибут source
 img.src = 'https://picsum.photos/240';
-//Добавить атрибут width=240
+//I.2. Добавить атрибут width=240
 img.width = 240;
-//добавить класс super
+//I.3. добавить класс super
 img.classList.add('super');
-
+//I.4. Добавить свойство alt со значением 'Super Man'
 img.alt = 'Super Man'
+
+//Поместить изоюражение внутрь элемента с классом wrapper
+div.appendChild(img);
+//Используя Html строку, создать DIV с классом 'pDiv' + с 2мя параграфами
+const elemHTML = `
+<div class = 'pDiv'>
+    <p>Параграф 1</p>
+    <p>Параграф 2</p>
+</div>`
+
+//Поместить этот DIV до элемента <ul></ul>
+const ulList = div.querySelector('ul');
+ulList.insertAdjacentHTML('beforebegin', elemHTML);//добавляем разметку до списка ul
+
+//Добавить для второго пораграфа класс text
+const pDiv = document.querySelector('.pDiv');
+pDiv.children[1].classList.add('text');
+
+//Удалить первый параграф
+pDiv.firstElementChild.remove();
+
+//Создать функцию generateAutoCard, которая будет принимать три аргумента: brand, color, year
+//Функция должна возвращать разметку HTML вида:
+/*
+    <div class="autoCard">
+        <h2> BRAND YEAR </h2>
+        <p> Автомобиль BRAND - YEAR года. Возраст авто - YEARS лет.</p>
+    </div>
+*/
+const generateAutoCard = (brand, color, year) =>
+{
+    return`
+        <div class="autoCard">
+            <h2> BRAND YEAR </h2>   
+            <p> Автомобиль BRAND - YEAR года. Возраст авто - YEARS лет.</p>
+        </div>
+    `;
+}
+
+//Создать новый div с классом autos
+const carsDiv = document.createElement('div');
+carsDiv.classList.add('autos');//добавляем класс autos
+
+//Создать 3 карточки авто, используя функцию generateAutoCard
+const carsList = [
+    {brand: 'Tesla', year: 2015, color: 'red'},
+    {brand: 'Lexus', year: 2016, color: 'silver'},
+    {brand: 'Nissan', year: 2012, color: 'black'},
+];
+
+const carsHTML = carsList.map(car => {
+    return generateAutoCard(car.brand, car.year, car.color );
+}).join('');
+
+
+//Поместить 3 карточки внутрь DIV с классом autos
+carsDiv.innerHTML = carsHTML;
+
+//Поместить
+
+
+
+console.log(carsDiv);
